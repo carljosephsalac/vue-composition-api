84658cf7ca793fdfee197c1e757746eb98218bca
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-08-12 22:51:51 +0800

    Install Tailwind and daisyUI

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/" class="link link-info">Home</RouterLink>
     </nav>
   </header>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,24 +5,10 @@ const count = ref(0)
 const state = reactive({
   count: 0
 })
-const myClass = 'mb-5 block text-green'
+const myClass = 'btn btn-sm btn-success text-white block mb-3'
 </script>
 
 <template>
   <button @click="count++" :class="myClass">ref {{ count }}</button>
   <button @click="state.count++" :class="true && myClass">reactive {{ state.count }}</button>
 </template>
-
-<style>
-.block {
-  display: block;
-}
-
-.mb-5 {
-  margin-bottom: 5px;
-}
-
-.text-green {
-  color: green;
-}
-</style>
