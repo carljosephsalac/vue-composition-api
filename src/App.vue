1966054d7fb5bbc391a7a92ebc6931f0c0bc893d
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-08-13 19:55:21 +0800

    Vue Router

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,28 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav class="flex justify-center">
-      <RouterLink to="/" class="link link-info">Home</RouterLink>
+  <header class="navbar bg-neutral text-neutral-content">
+    <div class="flex-1">
+      <RouterLink to="/" class="text-xl btn btn-ghost">daisyUI</RouterLink>
+    </div>
+    <nav class="flex-none">
+      <ul class="px-1 menu menu-horizontal">
+        <li>
+          <RouterLink to="/posts/create">Create Post</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/posts/edit">Edit Post</RouterLink>
+        </li>
+        <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="p-2 rounded-t-none bg-base-100">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </nav>
   </header>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import PostCreate from '@/views/PostCreate.vue'
+import PostEdit from '@/views/PostEdit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +10,17 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },   
+    },
+    {
+      path: '/posts/create',
+      name: 'posts.create',
+      component: PostCreate
+    },
+    {
+      path: '/posts/edit',
+      name: 'posts.edit',
+      component: PostEdit
+    }
   ]
 })
 
